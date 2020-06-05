cbdd774d453f4e7e62943a6e83fc05a38c027b7b
mendoza <femoxmed@gmail.com>
2020-06-05 02:57:58 +0100

    First commit

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <div className="container">
+            <div>Footer</div>
+
+
+            <style jsx>{`
+       
+      `}</style>
+
+            <style jsx global>{`
+       
+      `}</style>
+        </div>
+    )
+}
---
+++ components/layout/Default.js
@@ -0,0 +1,44 @@
+// import Link from "next/link";
+
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+
+export default function (props) {
+
+  return (
+    <div className="">
+      <Navbar />
+      <div className="width-full overflow-auto">{props.children}</div>
+      <Footer />
+
+      <style jsx>{`
+        #nav-container {
+          // position: fixed;
+          font-size: 5px;
+          position: fixed;
+          top: 30px;
+          left: 10px;
+          z-index: 7;
+        }
+        .top {
+          top: 0px;
+        }
+        .width-full {
+          width: -web-kit-available-;
+          z-index: 1;
+        }
+        .getUp {
+          z-index: 6;
+        }
+        .smoke-screen {
+          background: #e5e5e5;
+          min-height: 100vh;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+// export const Layout = page => <Default>{page}</Default>;
+
